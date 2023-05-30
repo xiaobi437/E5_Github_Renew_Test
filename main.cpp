5bf345037bec2b9bed38149e125149ac21405ffd
bi xiao <Xiaobi437@163.com>
2023-05-30 21:51:17 +0800

    new_23.5.30

--- main.cpp
+++ main.cpp
@@ -4,15 +4,7 @@
 
 int main(int argv, char *argc[]) {
 
-	Person Tom("Tom","man",10,666);
-	Tom.Person_show();
 
-	Game_Player Jack("Jack", "man", 10, 666,99);
-	Jack.Game_Player_show();
-
-	Game_Player Rose;
-	Rose.Game_Player_set("Rose", "woman", 12, 666, 99);
-	Rose.Game_Player_show();
 
 	cout << "-----------------------" << endl;
 	Shape get_shape;
